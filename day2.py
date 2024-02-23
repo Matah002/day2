24cef8f83979fe05182ecb587a73838c641fac12
Matah002 <jameskimata397@gmail.com>
2024-02-24 00:33:26 +0300

    init

--- day2.py
+++ day2.py
@@ -4,3 +4,26 @@ print(3*2)
 a = 3
 b = 4
 print(a+b)
+
+integer_number = 123
+float_number = 1.23
+new_number = integer_number + float_number
+print(new_number)
+print(type(new_number))
+
+a=5
+b=8
+print(a>b)
+
+num = input("Enter a number:")
+
+print("You entered:", num)
+
+print("Data type of num:", type(num))
+
+
+name = input("What is your name:")10
+age = input("How old are you:")
+location = input("where do you live:")
+
+print("Hello"+ name + "you are" + age + "years old and live in" + location )
