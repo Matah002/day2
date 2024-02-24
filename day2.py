c45ed50cad9f9bf8f366e87c892f2490fe0936d8
Matah002 <jameskimata397@gmail.com>
2024-02-24 11:34:57 +0300

    init

--- day2.py
+++ day2.py
@@ -15,6 +15,7 @@ a=5
 b=8
 print(a>b)
 
+"""
 num = input("Enter a number:")
 
 print("You entered:", num)
@@ -22,8 +23,27 @@ print("You entered:", num)
 print("Data type of num:", type(num))
 
 
-name = input("What is your name:")10
+name = input("What is your name:")
 age = input("How old are you:")
 location = input("where do you live:")
 
 print("Hello"+ name + "you are" + age + "years old and live in" + location )
+
+"""
+a = (1, 2, 3)
+
+b = list(a)
+
+print(b)
+
+a = 10
+
+b = 3
+
+print(a % b)
+
+
+x = {}
+x[2] = 10
+x[1] = [20,30,40]
+print(x[1][2])
