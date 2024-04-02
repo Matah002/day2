6ca7db8405b427cfc88d023307a9538523980e20
Matah002 <jameskimata397@gmail.com>
2024-04-02 17:15:56 +0300

    init

--- file_handling_assignment.py
+++ file_handling_assignment.py
@@ -1,3 +1,11 @@
-f = open("C:\Users\ASUS\OneDrive\Desktop\day1\my_file.txt", "r")
+f = open("C:\\Users\\ASUS\\OneDrive\\Desktop\\day1\\my_file.txt", "r")
 print(f.read())
+
+with open("C:\\Users\\ASUS\\OneDrive\\Desktop\\day1\\my_file.txt", "r+")as file:
+    contents = file.read()
+    file.write("I am 19 years old.")
+
+
+
+
 
